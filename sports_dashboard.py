027e50e13d6b7a903bb37bb07d0795bf9638ce84
jeslynsee <jeslynesee@gmail.com>
2026-05-10 21:35:10 -0700

    Display image and article text body on sports dashboard

--- sports_dashboard.py
+++ sports_dashboard.py
@@ -13,6 +13,14 @@ bball_image_url, ftball_image_url, bsball_image_url, tenn_image_url, wnba_image_
 class SportsDashboard(QWidget):
     go_back = Signal()
 
+    # helper function to load images using QPixmap 
+    def load_image_from_url(self, url):
+        data = urlopen(url).read()
+        pixmap = QPixmap()
+        pixmap.load_from_data(data)
+        return pixmap
+
+
     def __init__(self):
         super().__init__()
 
@@ -41,7 +49,7 @@ class SportsDashboard(QWidget):
         search_bar.add_widget(search_btn)
         no_scroll_layout.add_layout(search_bar)
 
-        # defining CSS style sheet for the article cards to apply below
+       # defining CSS style sheet for the article cards to apply below
         card_style = """
             QWidget#article_card {
                 border: 1px solid #b0b0b0;
@@ -54,83 +62,124 @@ class SportsDashboard(QWidget):
             }
         """
 
+        # hardcoded start and end of text body to give excerpt style display 
+        
+        # basketball card - image on left, title and excerpt on right
         head1 = QLabel(bball_text_title)
         hf1 = head1.font
         hf1.set_point_size(15)
         hf1.set_bold(True)
         head1.font = hf1
-        body1 = QLabel('Short excerpt placeholder.')
+        img1 = QLabel()
+        pixmap1 = self.load_image_from_url(bball_image_url)
+        img1.pixmap = pixmap1.scaled(200, 120, Qt.KeepAspectRatio)
+        body1 = QLabel(bball_article_text[97:300] + '...')
         body1.word_wrap = True
         card1 = QWidget()
         card1.object_name = 'article_card'
         card1.style_sheet = card_style
-        inner1 = QVBoxLayout()
-        inner1.add_widget(head1)
-        inner1.add_widget(body1)
+        inner1 = QHBoxLayout()
+        text_side1 = QVBoxLayout()
+        text_side1.add_widget(head1)
+        text_side1.add_widget(body1)
+        inner1.add_widget(img1)
+        inner1.add_layout(text_side1)
         card1.set_layout(inner1)
         vbox.add_widget(card1)
 
+        # football card - image on left, title and excerpt on right
         head2 = QLabel(ftball_text_title)
         hf2 = head2.font
         hf2.set_point_size(15)
         hf2.set_bold(True)
         head2.font = hf2
-        body2 = QLabel('Second excerpt placeholder.')
+        img2 = QLabel()
+        pixmap2 = self.load_image_from_url(ftball_image_url)
+        img2.pixmap = pixmap2.scaled(200, 120, Qt.KeepAspectRatio)
+        body2 = QLabel(ftball_article_text[41:300] + '...')
+        body2.word_wrap = True
         card2 = QWidget()
         card2.object_name = 'article_card'
         card2.style_sheet = card_style
-        inner2 = QVBoxLayout()
-        inner2.add_widget(head2)
-        inner2.add_widget(body2)
+        inner2 = QHBoxLayout()
+        text_side2 = QVBoxLayout()
+        text_side2.add_widget(head2)
+        text_side2.add_widget(body2)
+        inner2.add_widget(img2)
+        inner2.add_layout(text_side2)
         card2.set_layout(inner2)
         vbox.add_widget(card2)
 
+        # baseball card - image on left, title and excerpt on right
         head3 = QLabel(bsball_text_title)
         hf3 = head3.font
         hf3.set_point_size(15)
         hf3.set_bold(True)
         head3.font = hf3
-        body3 = QLabel('Short excerpt placeholder.')
+        img3 = QLabel()
+        pixmap3 = self.load_image_from_url(bsball_image_url)
+        img3.pixmap = pixmap3.scaled(200, 120, Qt.KeepAspectRatio)
+        body3 = QLabel(bsball_article_text[78:300] + '...')
+        body3.word_wrap = True
         card3 = QWidget()
         card3.object_name = 'article_card'
         card3.style_sheet = card_style
-        inner3 = QVBoxLayout()
-        inner3.add_widget(head3)
-        inner3.add_widget(body3)
+        inner3 = QHBoxLayout()
+        text_side3 = QVBoxLayout()
+        text_side3.add_widget(head3)
+        text_side3.add_widget(body3)
+        inner3.add_widget(img3)
+        inner3.add_layout(text_side3)
         card3.set_layout(inner3)
         vbox.add_widget(card3)
 
+        # tennis card - image on left, title and excerpt on right
         head4 = QLabel(tenn_text_title)
         hf4 = head4.font
         hf4.set_point_size(15)
         hf4.set_bold(True)
         head4.font = hf4
-        body4 = QLabel('Short excerpt placeholder.')
+        img4 = QLabel()
+        pixmap4 = self.load_image_from_url(tenn_image_url)
+        img4.pixmap = pixmap4.scaled(200, 120, Qt.KeepAspectRatio)
+        body4 = QLabel(tenn_article_text[76:300] + '...')
+        body4.word_wrap = True
         card4 = QWidget()
         card4.object_name = 'article_card'
         card4.style_sheet = card_style
-        inner4 = QVBoxLayout()
-        inner4.add_widget(head4)
-        inner4.add_widget(body4)
+        inner4 = QHBoxLayout()
+        text_side4 = QVBoxLayout()
+        text_side4.add_widget(head4)
+        text_side4.add_widget(body4)
+        inner4.add_widget(img4)
+        inner4.add_layout(text_side4)
         card4.set_layout(inner4)
         vbox.add_widget(card4)
 
+        # wnba card - image on left, title and excerpt on right
         head5 = QLabel(wnba_text_title)
         hf5 = head5.font
         hf5.set_point_size(15)
         hf5.set_bold(True)
         head5.font = hf5
-        body5 = QLabel('Short excerpt placeholder.')
+        img5 = QLabel()
+        pixmap5 = self.load_image_from_url(wnba_image_url)
+        img5.pixmap = pixmap5.scaled(200, 120, Qt.KeepAspectRatio)
+        body5 = QLabel(wnba_article_text[98:300] + '...')
+        body5.word_wrap = True
         card5 = QWidget()
         card5.object_name = 'article_card'
         card5.style_sheet = card_style
-        inner5 = QVBoxLayout()
-        inner5.add_widget(head5)
-        inner5.add_widget(body5)
+        inner5 = QHBoxLayout()
+        text_side5 = QVBoxLayout()
+        text_side5.add_widget(head5)
+        text_side5.add_widget(body5)
+        inner5.add_widget(img5)
+        inner5.add_layout(text_side5)
         card5.set_layout(inner5)
         vbox.add_widget(card5)
 
-        # created container and scroll view, so this page is scrollable (using QScrollView)
+        # created container and scroll view, so this page is scrollable (using QScrollArea)
         container = QWidget()
         container.set_layout(vbox)
         container.minimum_height = 1000
@@ -143,7 +192,7 @@ class SportsDashboard(QWidget):
         outer.add_layout(no_scroll_layout)
         outer.add_widget(scroll)
         self.set_layout(outer)
-        self.resize(800, 300)
+        self.resize(800, 600)
         # self.show()
 
 
